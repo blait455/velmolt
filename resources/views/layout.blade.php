2059e8da747603c3314144fcbb9746ca2ebe85f3
blait455 <blait455@gmail.com>
2021-03-26 21:47:41 +0100

    tawk.to integration

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -222,6 +222,20 @@
         toastr.warning("{{ session('alert') }}");
         </script>
     @endif
+
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/605e3a84067c2605c0bcab96/1f1o195tc';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
 </body>
 
 </html>
